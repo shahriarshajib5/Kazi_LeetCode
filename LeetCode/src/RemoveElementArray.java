9e80986e97472d87fd73462e7d7ff9f746f14140
shahriarshajib5 <S555438@nwmissouri.edu>
2023-12-16 13:34:14 -0600

    RomoveElementArray

--- LeetCode/src/RemoveElementArray.java
+++ LeetCode/src/RemoveElementArray.java
@@ -41,16 +41,27 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 
 
     int count = 0;
-    int[] myArray = new int[nums.length];
+    
     for(int i=0; i<nums.length; i++)
     {
         if (nums[i]!=val)
         {
-           myArray[count] = nums[i];
            count++;
            
         }
     }
+    int[] myArray = new int[count];
+    
+    int index=0;
+    for(int i=0; i<nums.length; i++)
+    {
+         if (nums[i]!=val)
+        {
+           myArray[index] = nums[i];
+           index++;
+        }
+    }
+    
      
         System.out.println(count);
         
